d1540170d55625b7649d3a7c1338304f53080d5b
Z-HNAN <zhn68033@163.com>
2019-10-10 00:10:08 +0800

    [router] 修复eslint的问题

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', redirect: { name: 'index' }},
+    { path: '/', redirect: { name: 'index' } },
     /* 首页相关 */
     { name: 'index', path: '/index', component: Index },
 
