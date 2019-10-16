2332d1947e6533e3e8d09c7c93bf07f1948a9994
Z-HNAN <zhn68033@163.com>
2019-10-16 15:37:49 +0800

    feat 增加个人账户共享模块静态页面

--- src/router/Share/index.vue
+++ src/router/Share/index.vue
@@ -1,95 +1,59 @@
 <template>
   <div class="main">
-    <el-row >
-      <el-col :span="12">
-        <div class="grid-content divLeft ">
-           <h1>个人账户</h1>
-           <el-row v-for="(item, index) in person" :key="index">
-             <el-col :span="5">
-               <div class="myIcon">
-                 <span class="fa fa-user fa-5x fa-fw"></span>
-               </div>
-             </el-col>
-             <el-col :span="9">{{item}}</el-col>
-             <el-col :span="10">
-               <el-button type="danger" @click="isDelete">删除</el-button>
-               <el-button type="primary">查看</el-button>
-             </el-col>
-           </el-row>
-        </div>
-      </el-col>
-      <el-col :span="12">
-        <div class="grid-content ">
-          <h1>共享账户</h1>
-          <el-row v-for="(item, index) in shares" :key="index">
-            <el-col :span="5">
-              <div class="myIcon myIcon2">
-                <span class="fa fa-4x fa-users fa-fw"></span>
-              </div>
-            </el-col>
-            <el-col :span="9">{{item}}</el-col>
-            <el-col :span="10">
-              <el-button type="danger">删除</el-button>
-              <el-button type="primary">查看</el-button>
-            </el-col>
+    <el-row>
+      <el-col :span="12" class="list">
+        <div>
+          <h1>个人账户</h1>
+          <el-row v-for="item in person" :key="item.id">
+            <AccountCard :isShare='false' :account='item' @delete='handleDelete' @detail='handelDetail'/>
           </el-row>
         </div>
+      </el-col>
+      <el-col :span="12" class="list">
+        <h1>共享账户</h1>
+        <el-row v-for="item in shares" :key="item.id">
+          <AccountCard :isShare='true' :account='item' @delete='handleDelete' @detail='handelDetail'/>
+        </el-row>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import AccountCard from '@/components/AccountCard/index.vue'
+
 export default {
   data () {
     return {
-      person: ['我的账户1', '我的账户2', '我的账户3'],
-      shares: ['家庭共享账户', '寝室共享账户', '公司共享账户']
+      person: [{name: '我的账户1', id: '1'}, {name: '我的账户2', id: '2'}, {name: '我的账户3', id: '3'}],
+      shares: [{name: '家庭共享账户', id: '4'}, {name: '寝室共享账户', id: '5'}, {name: '公司共享账户', id: '6'}]
     }
   },
   methods: {
-    isDelete: function () {
-      this.$confirm('确认删除?', '提示', {})
+    /**
+     * 组件触发删除
+     */
+    handleDelete (accountId) {
+      this.$confirm(`确认删除? ${accountId}`, '提示', {})
         .then(() => {
-          sessionStorage.setItem('user', this.ruleForm.username)
-          this.$router.push({path: '/'})
+          console.log(`删除了账户 ${accountId}`)
         })
         .catch(() => { })
+    },
+    /**
+     * 组件触发查看详情
+     */
+    handelDetail (accountId) {
+      console.log(`查看了账户 ${accountId}`)
     }
+  },
+  components: {
+    AccountCard
   }
 }
 </script>
-
 <style scoped>
-  .main .grid-content .el-row{
-    width: 100%;
-    height: 100px;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-    line-height: 100px;
-    font-size: 18px;
-  }
-  .divLeft {
-    margin-right: 20px;
-  }
-
-  h1{
-    margin-bottom: 30px;
-  }
-
-  .myIcon {
-    width: 100%;
-    height: 100px;
-    background-color: rgba(0,0,0,0.1);
-  }
-
-  .myIcon2 {
-    padding-top: 12px;
-    height: 88px;
-  }
-
-  .el-row{
-    margin-bottom: 30px;
-  }
-
+.list {
+  padding: 0 20px;
+}
 </style>
