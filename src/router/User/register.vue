720a51c99dd565eaea5d6518cf157a99f494dbfd
Yolo9 <1670131315@qq.com>
2019-12-04 18:55:47 +0800

    feat添加了注册组件

--- src/router/User/register.vue
+++ src/router/User/register.vue
@@ -37,7 +37,7 @@
                 class="rememberme"
             >记住密码</el-checkbox>
             <el-form-item style="width:100%;">
-                <el-button type="primary" plain style="width:100%;" @click="handleSubmit" :loading="logining">登录</el-button>
+                <el-button type="primary" plain style="width:100%;" @click="handleSubmit" :loading="logining">注册</el-button>
             </el-form-item>
         </el-form>
     </div>
